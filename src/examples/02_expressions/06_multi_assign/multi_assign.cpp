f63ec9d11ff1819152da877caa39ad51e0126008
vnels <vmnelsonrr@gmail.com>
2022-06-02 20:55:17 -0500

    Assignment 2

--- src/examples/02_expressions/06_multi_assign/multi_assign.cpp
+++ src/examples/02_expressions/06_multi_assign/multi_assign.cpp
@@ -1,3 +1,5 @@
+#include "multi_assign.h"
+
 /*
 Function multi_assign_addition creates three int variables num1, num2, and num3 and
 sets their value to incoming parameters, and return the sum of the three variables, 
@@ -5,3 +7,13 @@ sets their value to incoming parameters, and return the sum of the three variabl
 @param int num
 @return the sum of num three times 
 */
+
+int multiply_numbers (int num1)
+{
+    auto num2 =5;
+    return num1 * num2;
+}
+
+
+
+// int multi_assign_addition
